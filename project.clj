c9d38fe7424eb6bc189d02041e012a308b496d2e
Dennis Crenshaw <crenshawda@gmail.com>
2010-09-29 14:20:52 -0400

    Initializing barebones glutton project infrastructure.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject glutton "0.0.1"
+  :description "Fast, efficient, extensible genome digestion algorithm"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [com.stuartsierra/lazytest "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :repositories {"stuartsierra.com" "http://stuartsierra.com/m2snapshots"})
