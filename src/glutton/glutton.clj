adcebbc5bdd43038a2577cfd310e6f2a6c80cd3e
Christopher Maier <maier@med.unc.edu>
2010-10-01 12:51:30 -0400

    Some tweaks and cleanup

--- src/glutton/glutton.clj
+++ src/glutton/glutton.clj
@@ -1,9 +1,7 @@
 (ns glutton.glutton
-  (:use [clojure.contrib.seq-utils :only [indexed]])
-  (:use [clojure.contrib.duck-streams :only [read-lines]]))
+  (:use [clojure.contrib.seq-utils :only [indexed]]))
 
-(def begin-flag :START)
-(def breaks #{:K :R :. begin-flag}) ; Consider Proline
+(def breaks #{:K :R :. nil}) ; Consider Proline
 (def starts #{:M})
 (def max-breaks 2)
 
@@ -16,20 +14,21 @@
                 (if (break? current-aa)
                   (update-in c [:breaks] inc)
                   c)))
-            (if (or (break? prev-aa)
-                    (start? current-aa))
-              [{:sequence [current-aa] :breaks 0 :nucleotide-start (* 3 loc)}])))
+            (if (or (break? prev-aa)    ;after you cleave, you need to begin a new one
+                    (start? current-aa) ;obviously
+                    (= :M prev-aa))     ;N-terminal methionines often get removed
+                                        ; (TODO: does this happen for all organisms?)
+                     [{:sequence [current-aa] :breaks 0 :nucleotide-start (* 3 loc)}])))
 
 (defn- digest*
   [[[loc [prev-aa current-aa]] & other-aas :as aas] candidates]
-  (if (seq other-aas)
-    (let [candidates (process candidates current-aa prev-aa loc)]
+  (let [new-candidates (process candidates current-aa prev-aa loc)]
+    (if (seq other-aas)
       (if-not (break? current-aa)
-        (recur other-aas candidates)
-        (lazy-cat candidates (digest* other-aas (remove #(> (:breaks %) max-breaks)
-                                                        candidates)))))
-    (process candidates current-aa prev-aa loc)))
+        (recur other-aas new-candidates)
+        (lazy-cat new-candidates (digest* other-aas (remove #(> (:breaks %) max-breaks)
+                                                        new-candidates))))
+      new-candidates)))
 
-(defn digest
-  [aas]
-  (digest* (indexed (partition 2 1 (cons begin-flag aas))) []))
+(defn digest [aas]
+  (digest* (indexed (partition 2 1 (cons nil aas))) []))
