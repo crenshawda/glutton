dc8a87f1895f626a77013ecac344f53a69f32e7b
Dennis Crenshaw <crenshawda@gmail.com>
2010-10-04 16:16:32 -0400

    Added mass to Peptide record and peptide mass cumulation functionality to process.

--- src/glutton/glutton.clj
+++ src/glutton/glutton.clj
@@ -1,18 +1,23 @@
 (ns glutton.glutton
-  (:use [clojure.contrib.seq-utils :only [indexed]]))
+  (:use [clojure.contrib.seq-utils :only [indexed]])
+  (:require (glutton [lexicon :as lex])))
 
-(defrecord Peptide [sequence breaks nucleotide-start])
+(defrecord Peptide [sequence breaks nucleotide-start mass])
 
 (def breaks #{:K :R :. nil}) ; Consider Proline
 (def starts #{:M})
 (def max-breaks 2)
+(def mass-type :monoisotopic-mass)
 
 (defn- start? [aa] (contains? starts aa))
 (defn- break? [aa] (contains? breaks aa))
 
+(defn- get-mass [mass-type amino-acid] (mass-type (lex/*amino-acid-dictionary* amino-acid)))
 (defn- process [candidates current-aa prev-aa loc]
   (lazy-cat (for [c candidates]
-              (let [c (update-in c [:sequence] conj current-aa)]
+              (let [c (-> c
+                          (update-in [:sequence] conj current-aa)
+                          (update-in [:mass] + (get-mass mass-type current-aa)))]
                 (if (break? current-aa)
                   (update-in c [:breaks] inc)
                   c)))
@@ -20,7 +25,7 @@
                     (start? current-aa) ;obviously
                     (= :M prev-aa))     ;N-terminal methionines often get removed
                                         ; (TODO: does this happen for all organisms?)
-              [(Peptide. [current-aa] 0 (* 3 loc))])))
+              [(Peptide. [current-aa] 0 (* 3 loc) 0)])))
 
 (defn- digest*
   [[[loc [prev-aa current-aa]] & other-aas :as aas] candidates]
