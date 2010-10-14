8c221d70d6ae44085ea5706bffc369aaa4ece144
Dennis Crenshaw <crenshawda@gmail.com>
2010-10-14 16:05:45 -0400

    Updating testing dependencies. We should eventually get back to the cannon release [com.stuartsierra/lazytest "1.x.x"]

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject glutton "0.0.1"
   :description "Fast, efficient, extensible genome digestion algorithm"
   :dependencies [[org.clojure/clojure "1.2.0"]
-		 [org.clojure/clojure-contrib "1.2.0"]
-		 [com.stuartsierra/lazytest "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
-  :repositories {"stuartsierra.com" "http://stuartsierra.com/m2snapshots"}
+		 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [org.clojars.ninjudd/lazytest "1.1.3-SNAPSHOT"]]
   :jvm-opts ["-server"
              "-Xmx1g"])
