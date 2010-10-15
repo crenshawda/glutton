8673b2a6d6cc14b9951291b3581625a4c174a588
Christopher Maier <maier@med.unc.edu>
2010-10-15 10:32:49 -0400

    Little cleanup of enzyme-free digester code

--- src/glutton/enzyme_free.clj
+++ src/glutton/enzyme_free.clj
@@ -3,16 +3,12 @@
         [glutton [peptide-record]])
   (:import [glutton.peptide-record Peptide]))
 
-(defn create-peptide [aa position]
-  "Constructor creates peptide with 0 breaks, as they aren't important in this algorithm"
-  (Peptide. [aa] position (aa-mass aa) 0))
-
 (defn- all-sub-peptides [[[position first-aa] & other-aas]]
   (if (seq? other-aas)
     (lazy-cat [(reductions (fn [p [pos aa]] (extend-with p aa))
                            (create-peptide first-aa (* position 3)) other-aas)]
               (all-sub-peptides other-aas))
-    [[(create-peptide first-aa position)]]))
+    [[(initiate-peptide first-aa position)]]))
 
 (defn digest
   "Return all sub-peptides of a translation of a nucleotide sequence whose masses fall within
