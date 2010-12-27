8abf478360817b008ffc3e7376f3a21bf3f4d072
Dennis Crenshaw <crenshawda@Labtop.local>
2010-12-27 00:49:58 -0500

    Added a simple tab-delimited exporter.

--- src/glutton/file_utils.clj
+++ src/glutton/file_utils.clj
@@ -8,3 +8,14 @@
              (partition-by #(.startsWith % ">")
                            (line-seq (reader file)))))
 
+(defn ->tab-delimited
+  "Outputs peptide records into a simple tab-delimited format"
+  ([peptide-records]
+     (->tab-delimited peptide-records "output.txt"))
+  ([peptide-records filename]
+     ;; Doesn't check to see if the file already exists before appending
+     (with-open [w (writer filename :append true)]
+       (doseq [frame peptide-records]
+         (doseq [record frame]
+           (.write w
+                 (str (apply str (interpose "\t" (vals record))) "\n")))))))
