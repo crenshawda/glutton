ae4e6b3afddf8386211756a84ea451d5da0d5308
Dennis Crenshaw <crenshawda@Labtop.local>
2010-12-27 00:49:36 -0500

    Moved fasta stuff to genomic-utils since that's where most of the heavy lifting happens.

--- src/glutton/file_utils.clj
+++ src/glutton/file_utils.clj
@@ -8,18 +8,3 @@
              (partition-by #(.startsWith % ">")
                            (line-seq (reader file)))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                        ; Fasta file functions
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn partition-multiseq-fasta
-  "Give this a sequence of fasta files and it will parse out the important stuff for each"
-  [fasta-lines]
-  (partition-by #(.startsWith % ">") fasta-lines))
-
-
-; TODO: Refactor ALL this to the right places
-(defn single-fasta
-  [fasta-file-str]
-  ; TODO: (first because multiseq gives back a list of 1?
-  (let [[head seq] (partition-multiseq-fasta (flatten (ds/read-lines fasta-file-str)))]
-    (gu/parse-fasta head seq)))
