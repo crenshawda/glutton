81cb378d92f2fc24679b7919ab20377a59aba4b7
Dennis Crenshaw <crenshawda@Labtop.local>
2010-12-27 00:49:15 -0500

    Most things here were replaced by file->records.

--- src/glutton/file_utils.clj
+++ src/glutton/file_utils.clj
@@ -1,26 +1,12 @@
 (ns glutton.file-utils
-  (:require (clojure.contrib [duck-streams :as ds])
-            (glutton [genomic-utils :as gu])))
+  [:use
+   [clojure.java.io :only [writer]]])
 
-(defn file-roller
-  "Give it a directory and it will give a list of all the files within"
-  [directory]
-  (file-seq (new java.io.File directory)))
-
-(defn suffix-filter
-  "Give this a list of files and a suffix it will filter on it"
-  [file-list suffix]
-  (filter #(.endsWith (.getName %) suffix) file-list))
-
-(defn get-fasta-files
-  "Give this a directory string and it will grab all the fasta files out of that directory. This is a convenience composition of the file-roller and suffix-filter methods."
-  [dir-string]
-  (suffix-filter (file-roller dir-string) ".fasta"))
-
-(defn lines-from-files
-  "Give this a list of files and it will return a list of each files lines"
-  [files]
-  (pmap ds/read-lines files))
+;; THIS: Is stolen from `encode-mongo.import.varsplice
+(defn file->records [file]
+  (partition 2
+             (partition-by #(.startsWith % ">")
+                           (line-seq (reader file)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                         ; Fasta file functions
