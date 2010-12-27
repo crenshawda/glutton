60d944f677f80b5a05e6dba6b9e1ef1f0f44a006
Dennis Crenshaw <crenshawda@Labtop.local>
2010-12-27 00:53:49 -0500

    Updated core to reflect the changes in the underlying architecture.

--- src/glutton/core.clj
+++ src/glutton/core.clj
@@ -1,17 +1,24 @@
 ; Clojure implementation of the Giddings Lab GFS software
 ; Authors: Dennis Crenshaw, Chris Maier, Brian Risk
-; Date: September 28, 2010
+; Date: December 27, 2010
 
 (ns glutton.core
-    (:require (glutton [glutton :as glt]
-                       [genomic-utils :as gu]
-                       [file-utils :as file])))
+  [:require [glutton
+             [glutton :as glt]
+             [genomic-utils :as gu]
+             [file-utils :as file]]])
 
-(defn peptides-from-fasta-file
-  [file-string]
-  (pmap glt/digest (pmap gu/frame->amino-acids (:frames (file/single-fasta file-string)))))
+;; to-amino-acids should be done after digestion as an output option?
+(defn peptides-from-fasta
+  "Given a FASTA file-str, this generates peptides from the genome sequence"
+  [file-str]
+  (pmap glt/digest
+        (gu/to-amino-acids
+         (:frames
+          ;; This first is fragile-- it should be able to comprehend better than this!
+          (first (gu/single-fasta file-str))))))
 
 (defn peptide-count
-  "Given a file string, takes a fasta file and count generated peptides from the genome sequence"
-  [file-string]
-  (reduce + 0 (pmap count (peptides-from-fasta-file file-string))))
+  "Given a FASTA file-str, this counts peptides generated from the genome sequence"
+  [file-str]
+  (reduce + 0 (map count (peptides-from-fasta file-str))))
