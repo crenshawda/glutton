3009fb7b4456d2f249965734df5e8eb326d85e10
Dennis Crenshaw <crenshawda@gmail.com>
2011-02-17 15:03:48 -0500

    Upgraded to clojure 1.3 and friends, with the snazzy new swank server.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject glutton "0.0.1"
   :description "Fast, efficient, extensible genome digestion algorithm"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
+                 [org.clojure.contrib/standalone "1.3.0-alpha4"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [org.clojars.ninjudd/lazytest "1.1.3-SNAPSHOT"]]
   :jvm-opts ["-server"
              "-Xmx1g"])
