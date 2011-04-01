61ee1d58176f2b0064a8e7102f6da804ad843588
Christopher Maier <maier@med.unc.edu>
2011-04-01 15:43:22 -0400

    Dropping lazytest for now

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "Fast, efficient, extensible genome digestion algorithm"
   :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
                  [org.clojure.contrib/standalone "1.3.0-alpha4"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
-                     [org.clojars.ninjudd/lazytest "1.1.3-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :jvm-opts ["-server"
              "-Xmx1g"])
