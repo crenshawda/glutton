eed582f7a5d9a3a449d1dda65c6477cf78839ae9
Christopher Maier <maier@med.unc.edu>
2011-04-09 00:46:30 -0400

    Add some testing goodness

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
                  [org.clojure.contrib/standalone "1.3.0-alpha4"]
                  [robert/hooke "1.1.0"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-difftest "1.3.1"]
+                     [lein-retest "1.0.1"]
+                     [lein-test-out "0.1.0"]
+                     [swank-clojure "1.3.0-SNAPSHOT"]]
+  :hooks [leiningen.hooks.difftest
+          leiningen.hooks.retest]
   :test-selectors {:default (complement :integration)
                    :integration :integration
                    :all (constantly true)}
